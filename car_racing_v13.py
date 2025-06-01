99b916fece6a366d986ec23496bb621d8639e617
soaring189 <127698277+soaring189@users.noreply.github.com>
2025-06-02 04:31:16 +1200

    v12-v13

--- car_racing_v13.py
+++ car_racing_v13.py
@@ -1,4 +1,4 @@
-# This version of the game introduces a start menu, allowing users to freely select vehicles and adjust the difficulty.
+# This version of the game has rewritten the scoring logic, added a high score record, and converted the original score into mileage.
 import random
 import sys
 import tkinter as tk
@@ -9,27 +9,19 @@ from PIL import Image, ImageTk
 
 p.init()
 root = tk.Tk()
-root.title("Car Game")
-root.iconbitmap("./Assets/game_icon.ico")
-root.geometry("900x650")
-root.resizable(False, False)
-car_images_tk = [
-    "./Assets/car_1.png", "./Assets/car_2.png", "./Assets/car_3.png",
-    "./Assets/car_4.png", "./Assets/car_5.png", "./Assets/car_6.png"
-]
 car_attributes = [[3, 0.01, 5], [3.2, 0.012, 5.5], [3.8, 0.017, 6.5],
                   [2.5, 0.008, 4.5], [2.8, 0.009, 4.8], [3.5, 0.015, 6]]
 
 
-def select_car(index):
+def select_car(index, car_index, buttons):
     car_index.set(index)
     for button in buttons:
         button.config(bg="SystemButtonFace")
     buttons[index].config(bg="blue")
 
 
-def select_difficulty(level):
-    difficulty.set(level)
+def select_difficulty(level, challenge_level, easy_btn, normal_btn, hard_btn):
+    challenge_level.set(level)
     easy_btn.config(bg="systemButtonFace")
     normal_btn.config(bg="systemButtonFace")
     hard_btn.config(bg="systemButtonFace")
@@ -46,121 +38,84 @@ def close_window():
     sys.exit()
 
 
-car_index = tk.IntVar(value=0)
-buttons = []
-image_refs = []
-label_frame = tk.Frame(root, width=150, height=200)
-label_frame.place(x=30, y=200)
-tk.Label(label_frame, text="Lateral Speed", font=("Arial", 12, "bold")).place(
-    x=10, y=70)
-tk.Label(label_frame, text="Acceleration", font=("Arial", 12, "bold")).place(
-    x=10, y=100)
-tk.Label(label_frame, text="Max Speed", font=("Arial", 12, "bold")).place(x=10,
-                                                                          y=130)
-for image_id, image_path in enumerate(car_images_tk):
-    image = Image.open(image_path)
-    image = image.resize((80, 160))
-    photo = ImageTk.PhotoImage(image)
-    image_refs.append(photo)
-    img_x = 175 + image_id * 120
-    img_y = 100
-    tk.Label(root, image=photo).place(x=img_x, y=img_y)
-    attr_x = img_x + 15
-    tk.Label(root, text=f"{int(car_attributes[image_id][0] * 10)}",
-             font=("Arial", 10),
-             justify="center", width=6).place(x=attr_x, y=270)
-    tk.Label(root, text=f"{car_attributes[image_id][1] * 10}", font=("Arial", 10),
-             justify="center", width=6).place(x=attr_x, y=300)
-    tk.Label(root, text=f"{int(car_attributes[image_id][2] * 10)}",
-             font=("Arial", 10),
-             justify="center", width=6).place(x=attr_x, y=330)
-    btn = tk.Button(root, text="Choose",
-                    command=lambda index=image_id: select_car(index))
-    btn.place(x=img_x + 15, y=360)
-    buttons.append(btn)
-buttons[0].config(bg="blue")
-difficulty = tk.StringVar(value="Normal")
-difficulty_x = 300
-difficulty_y = 430
-easy_btn = tk.Button(root, text="Easy", width=10,
-                     command=lambda: select_difficulty("Easy"))
-easy_btn.place(x=difficulty_x, y=difficulty_y)
-normal_btn = tk.Button(root, text="Normal", width=10,
-                       command=lambda: select_difficulty("Normal"))
-normal_btn.place(x=difficulty_x + 120, y=difficulty_y)
-hard_btn = tk.Button(root, text="Hard", width=10,
-                     command=lambda: select_difficulty("Hard"))
-hard_btn.place(x=difficulty_x + 240, y=difficulty_y)
-normal_btn.config(bg="yellow")
-start_btn = ttk.Button(root, text="Start Game", command=root.destroy,
-                       style="Large.TButton")
-start_btn.place(x=450 - 75, y=500)
-style = ttk.Style()
-style.configure("Large.TButton", font=("Arial", 14, "bold"), padding=15)
-root.protocol("WM_DELETE_WINDOW", close_window)
-root.mainloop()
-screen_size = (700, 800)
-game_screen = p.display.set_mode(screen_size)
-p.display.set_caption("car game")
-icon = p.image.load("./Assets/game_icon.png").convert_alpha()
-p.display.set_icon(icon)
-bg_images = [
-    p.transform.smoothscale(p.image.load("./Assets/bg-0.png").convert_alpha(),
-                            (600, 800)),
-    p.transform.smoothscale(p.image.load("./Assets/bg-1.png").convert_alpha(),
-                            (600, 800)),
-    p.image.load("./Assets/bg-right.png").convert_alpha()
-]
-car_images = [
-    p.transform.smoothscale(p.image.load("./Assets/car_1.png").convert_alpha(),
-                            (80, 160)),
-    p.transform.smoothscale(p.image.load("./Assets/car_2.png").convert_alpha(),
-                            (80, 160)),
-    p.transform.smoothscale(p.image.load("./Assets/car_3.png").convert_alpha(),
-                            (80, 160)),
-    p.transform.smoothscale(p.image.load("./Assets/car_4.png").convert_alpha(),
-                            (80, 160)),
-    p.transform.smoothscale(p.image.load("./Assets/car_5.png").convert_alpha(),
-                            (80, 160)),
-    p.transform.smoothscale(p.image.load("./Assets/car_6.png").convert_alpha(),
-                            (80, 160))
-]
-number_list = []
-for i in range(0, 10):
-    number = p.image.load(f"./Assets/{i}.png").convert_alpha()
-    modified_number = p.transform.smoothscale(
-        p.image.load(f"./Assets/{i}.png").convert_alpha(), (48, 72))
-    number_list.append(modified_number)
-m_image = p.transform.smoothscale(
-    p.image.load(f"./Assets/m.png").convert_alpha(), (48, 48))
-speedometer_image = p.transform.smoothscale(
-    p.image.load("./Assets/speedometer.png").convert_alpha(), (100, 100))
-needle_image = p.transform.smoothscale(
-    p.image.load("./Assets/needle.png").convert_alpha(), (10, 60))
-circle_image = p.transform.smoothscale(
-    p.image.load("./Assets/circle.png").convert_alpha(), (10, 10))
-game_over_image = p.transform.smoothscale(
-    p.image.load("./Assets/game_over.png").convert_alpha(), (530.5, 74))
-restart_image = p.transform.smoothscale(
-    p.image.load("./Assets/restart_or_quit.png").convert_alpha(),
-    (377.5, 80.83))
-car_id = car_index.get()
-scroll_speed = 0
-collide_car_speed = 0
-score = 0
-needle_angle = 145
+def start_menu():
+    root.title("Car Game")
+    root.iconbitmap("./Assets/game_icon.ico")
+    root.geometry("900x650")
+    root.resizable(False, False)
+    car_images_tk = [
+        "./Assets/car_1.png", "./Assets/car_2.png", "./Assets/car_3.png",
+        "./Assets/car_4.png", "./Assets/car_5.png", "./Assets/car_6.png"
+    ]
+    car_index = tk.IntVar(value=0)
+    buttons = []
+    image_refs = []
+    label_frame = tk.Frame(root, width=150, height=200)
+    label_frame.place(x=30, y=200)
+    tk.Label(label_frame, text="Lateral Speed", font=("Arial", 12, "bold")).place(
+        x=10, y=70)
+    tk.Label(label_frame, text="Acceleration", font=("Arial", 12, "bold")).place(
+        x=10, y=100)
+    tk.Label(label_frame, text="Max Speed", font=("Arial", 12, "bold")).place(x=10,
+                                                                              y=130)
+    for image_id, image_path in enumerate(car_images_tk):
+        image = Image.open(image_path)
+        image = image.resize((80, 160))
+        photo = ImageTk.PhotoImage(image)
+        image_refs.append(photo)
+        img_x = 175 + image_id * 120
+        img_y = 100
+        tk.Label(root, image=photo).place(x=img_x, y=img_y)
+        attr_x = img_x + 15
+        tk.Label(root, text=f"{int(car_attributes[image_id][0] * 10)}",
+                 font=("Arial", 10),
+                 justify="center", width=6).place(x=attr_x, y=270)
+        tk.Label(root, text=f"{car_attributes[image_id][1] * 10}", font=("Arial", 10),
+                 justify="center", width=6).place(x=attr_x, y=300)
+        tk.Label(root, text=f"{int(car_attributes[image_id][2] * 10)}",
+                 font=("Arial", 10),
+                 justify="center", width=6).place(x=attr_x, y=330)
+        btn = tk.Button(root, text="Choose",
+                        command=lambda index=image_id: select_car(index, car_index, buttons))
+        btn.place(x=img_x + 15, y=360)
+        buttons.append(btn)
+    buttons[0].config(bg="blue")
+    challenge_level = tk.StringVar(value="Normal")
+    difficulty_x = 300
+    difficulty_y = 430
+    easy_btn = tk.Button(root, text="Easy", width=10,
+                         command=lambda: select_difficulty("Easy", challenge_level, easy_btn, normal_btn, hard_btn))
+    easy_btn.place(x=difficulty_x, y=difficulty_y)
+    normal_btn = tk.Button(root, text="Normal", width=10,
+                           command=lambda: select_difficulty("Normal", challenge_level, easy_btn, normal_btn, hard_btn))
+    normal_btn.place(x=difficulty_x + 120, y=difficulty_y)
+    hard_btn = tk.Button(root, text="Hard", width=10,
+                         command=lambda: select_difficulty("Hard", challenge_level, easy_btn, normal_btn, hard_btn))
+    hard_btn.place(x=difficulty_x + 240, y=difficulty_y)
+    normal_btn.config(bg="yellow")
+    start_btn = ttk.Button(root, text="Start Game", command=root.destroy,
+                           style="Large.TButton")
+    start_btn.place(x=450 - 75, y=500)
+    style = ttk.Style()
+    style.configure("Large.TButton", font=("Arial", 14, "bold"), padding=15)
+    root.protocol("WM_DELETE_WINDOW", close_window)
+    root.mainloop()
+    return car_index.get(), challenge_level.get()
 
-if difficulty.get() == "Easy":
-    FPS = 150
-    difficulty_num = 1
-elif difficulty.get() == "Normal":
-    FPS = 175
-    difficulty_num = 2
-else:
-    FPS = 200
-    difficulty_num = 3
-game_over = False
-running = True
+
+def read_high_score():
+    try:
+        with open("high_score.txt", "r") as file:
+            return int(file.read().strip())
+    except FileNotFoundError:
+        with open("high_score.txt", "w") as file:
+            file.write("0")
+        return 0
+
+
+def write_high_score(new_score):
+    with open("high_score.txt", "w") as file:
+        file.write(str(new_score))
 
 
 class Background:
@@ -258,9 +213,11 @@ class ObstacleCar:
         self.y = None
         self.car_num = None
         self.speed = None
+        self.passed = False
         self.reset_position(existing_cars)
 
     def reset_position(self, existing_cars):
+        self.passed = False
         occupied_lanes = [vehicle.x_pos_num for vehicle in existing_cars]
         free_lanes = []
         for lane in range(4):
@@ -285,6 +242,9 @@ class ObstacleCar:
 def game_state(bg_speed, obstacle_car_speed):
     global running, car
     if game_over:
+        if score > high_score:
+            write_high_score(score)
+
         game_screen.blit(game_over_image, (34.75, 363))
         if car.y < 800:
             car.y += bg_speed + obstacle_car_speed
@@ -300,8 +260,10 @@ def game_state(bg_speed, obstacle_car_speed):
 
 
 def reset_game():
-    global game_over, score, car, obstacle_cars, bg
+    global game_over, score, high_score, car, obstacle_cars, bg
     game_over = False
+    if score > high_score:
+        high_score = score
     score = 0
     car = Car(260, 600, car_id, car_attributes[car_id][0],
               car_attributes[car_id][1], car_attributes[car_id][2], car_images)
@@ -315,7 +277,7 @@ def reset_game():
 
 
 def display_stats(speed, max_speed):
-    global needle_angle, score
+    global needle_angle, distance, score
     game_screen.blit(bg_images[2], (600, 0))
     game_screen.blit(speedometer_image, (600, 700))
     game_screen.blit(circle_image, (645, 745))
@@ -328,13 +290,76 @@ def display_stats(speed, max_speed):
     rotated_rect = rotated_needle.get_rect(center=(650, 750))
     game_screen.blit(rotated_needle, rotated_rect.topleft)
     if not game_over:
-        score += 0.033 * scroll_speed * difficulty_num
-    for n in range(5):
-        game_screen.blit(number_list[(int(score) // (10 ** n)) % 10],
-                         (626, 475 - (n * 100)))
-    game_screen.blit(m_image, (626, 575))
+        distance += 0.033 * scroll_speed
+    for n in range(4):
+        game_screen.blit(number_list[(int(distance) // (10 ** n)) % 10],
+                         (626, 375 - (n * 100)))
+    game_screen.blit(m_image, (626, 475))
+    for n in range(3):
+        game_screen.blit(small_number_list[(high_score // (10 ** n)) % 10],
+                         (670 - (n * 30), 600))
+    for n in range(3):
+        game_screen.blit(small_number_list[(score // (10 ** n)) % 10],
+                         (670 - (n * 30), 650))
 
 
+car_id, difficulty = start_menu()
+screen_size = (700, 800)
+game_screen = p.display.set_mode(screen_size)
+p.display.set_caption("car game")
+p.display.set_icon(p.image.load("./Assets/game_icon.png").convert_alpha())
+bg_images = [
+    p.transform.smoothscale(p.image.load("./Assets/bg-0.png").convert_alpha(),
+                            (600, 800)),
+    p.transform.smoothscale(p.image.load("./Assets/bg-1.png").convert_alpha(),
+                            (600, 800)),
+    p.image.load("./Assets/bg-right.png").convert_alpha()
+]
+car_images = []
+for i in range(1, 7):
+    car_image = p.image.load(f"./Assets/car_{i}.png").convert_alpha()
+    car_images.append(p.transform.smoothscale(car_image, (80, 160)))
+number_list = []
+for i in range(0, 10):
+    number = p.image.load(f"./Assets/{i}.png").convert_alpha()
+    modified_number = p.transform.smoothscale(
+        p.image.load(f"./Assets/{i}.png").convert_alpha(), (48, 72))
+    number_list.append(modified_number)
+small_number_list = []
+for i in range(0, 10):
+    number = p.image.load(f"./Assets/{i}.png").convert_alpha()
+    modified_number = p.transform.smoothscale(
+        p.image.load(f"./Assets/{i}.png").convert_alpha(), (24, 36))
+    small_number_list.append(modified_number)
+m_image = p.transform.smoothscale(
+    p.image.load(f"./Assets/m.png").convert_alpha(), (48, 48))
+speedometer_image = p.transform.smoothscale(
+    p.image.load("./Assets/speedometer.png").convert_alpha(), (100, 100))
+needle_image = p.transform.smoothscale(
+    p.image.load("./Assets/needle.png").convert_alpha(), (10, 60))
+circle_image = p.transform.smoothscale(
+    p.image.load("./Assets/circle.png").convert_alpha(), (10, 10))
+game_over_image = p.transform.smoothscale(
+    p.image.load("./Assets/game_over.png").convert_alpha(), (530.5, 74))
+restart_image = p.transform.smoothscale(
+    p.image.load("./Assets/restart_or_quit.png").convert_alpha(),
+    (377.5, 80.83))
+high_score = read_high_score()
+distance = 0
+score = 0
+if difficulty == "Easy":
+    FPS = 150
+    difficulty_num = 1
+elif difficulty == "Normal":
+    FPS = 175
+    difficulty_num = 2
+else:
+    FPS = 200
+    difficulty_num = 3
+game_over = False
+scroll_speed = 0
+collide_car_speed = 0
+needle_angle = 145
 car = Car(260, 600, car_id, car_attributes[car_id][0],
           car_attributes[car_id][1], car_attributes[car_id][2], car_images)
 obstacle_cars = []
@@ -344,6 +369,7 @@ for i in range(difficulty_num):
     else:
         obstacle_cars.append(ObstacleCar(car_images, obstacle_cars))
 bg = Background(bg_images, scroll_speed)
+running = True
 while running:
     events = p.event.get()
     for event in events:
@@ -360,6 +386,10 @@ while running:
         if car.check_collision(obstacle_car):
             collide_car_speed = obstacle_car.speed
             game_over = True
+        if obstacle_car.y > screen_size[1]:
+            if not obstacle_car.passed and not game_over:
+                score += difficulty_num
+                obstacle_car.passed = True
     scroll_speed = game_state(scroll_speed, collide_car_speed)
     display_stats(scroll_speed, car.max_speed)
     p.display.update()
